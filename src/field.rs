145272564c41e353a9db68961d2c7436ad3b1df3
wataruiwabuchi <speeeedcubist@gmail.com>
2020-05-25 18:14:39 +0900

    一列埋まっているかを判定する関数is_filled_each_rowを作成し，そのテストも行った

--- src/field.rs
+++ src/field.rs
@@ -42,6 +42,32 @@ impl Field {
     pub fn get_width(&self) -> usize {
         self.width
     }
+
+    // 横列ごとにminoが揃っているかを判定し揃っている列のインデクスを返す
+    // 削除したかという情報と削除した列の情報を返す
+    pub fn is_filled_each_row(&self) -> Option<Vec<usize>> {
+        let mut filled_rows = Vec::new();
+
+        for h in 0..self.get_height() {
+            let mut not_filled = false;
+            for w in 0..self.get_width() {
+                if !self.blocks[h][w].filled {
+                    not_filled = true;
+                    break;
+                }
+            }
+
+            if !not_filled {
+                filled_rows.push(h);
+            }
+        }
+
+        if filled_rows.len() == 0 {
+            return None;
+        }
+
+        return Some(filled_rows);
+    }
 }
 
 #[cfg(test)]
@@ -60,5 +86,58 @@ mod tests {
             }
         }
         assert!(true);
+    }
+
+    #[test]
+    fn test_is_filled_each_row() {
+        let test_height = 5;
+        let test_width = 4;
+
+        struct TestCase {
+            name: String,
+            x: Vec<Vec<bool>>,
+            want: Option<Vec<usize>>,
+        };
+
+        let cases = vec![
+            TestCase {
+                name: "all false".to_string(),
+                x: vec![vec![false; test_width]; test_height],
+                want: None,
+            },
+            TestCase {
+                name: "all true".to_string(),
+                x: vec![vec![true; test_width]; test_height],
+                want: Some(vec![0, 1, 2, 3, 4]),
+            },
+            TestCase {
+                // 一部が埋まっている
+                name: "hand craft".to_string(),
+                x: vec![
+                    vec![true, true, true, true],
+                    vec![false, false, true, false],
+                    vec![false, true, true, false],
+                    vec![true, true, true, true],
+                    vec![false, false, false, false],
+                ],
+                want: Some(vec![0, 3]),
+            },
+        ];
+
+        for case in cases {
+            // blockがすべて埋まっていないかをテスト
+            let mut f = Field::new(test_height, test_width);
+            for h in 0..f.get_height() {
+                for w in 0..f.get_width() {
+                    f.blocks[h][w].filled = case.x[h][w];
+                }
+            }
+            match f.is_filled_each_row() {
+                Some(return_value) => {
+                    assert_eq!(Some(return_value), case.want, "case {}: failed", case.name)
+                }
+                None => assert_eq!(None, case.want, "failed {}", case.name),
+            }
+        }
     }
 }
