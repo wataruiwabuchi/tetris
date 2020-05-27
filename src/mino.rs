c89a47be8a8e1522b3a63feaf36383088a853851
wataruiwabuchi <speeeedcubist@gmail.com>
2020-05-27 22:16:49 +0900

    minoの定義方法を刷新

--- src/mino.rs
+++ src/mino.rs
@@ -1,28 +1,42 @@
 // TODO : メンバ変数を参照する関数の処理を共通化する方法を探す
-// 現状ではget_heightなどの全く同じ動作を行う関数をすべてのミノに対して実装している
+// 現状ではget_sizeなどの全く同じ動作を行う関数をすべてのミノに対して実装している
 // traitのデフォルト実装でこの部分を共通化できれば良いがtraitからはメンバ変数にアクセスできないのでその部分に実装するとエラーが出る
 
-pub struct Mino {
+pub trait Mino {
+    fn new() -> Self;
+    fn get_size(&self) -> usize;
+    fn get_shape(&self) -> &Vec<Vec<bool>>;
+    fn get_color(&self) -> [f32; 4];
+}
+
+pub struct TMino {
     size: usize,
     shape: Vec<Vec<bool>>,
     color: [f32; 4],
 }
 
-impl Mino {
-    pub fn get_size(&self) -> usize {
+impl Mino for TMino {
+    fn new() -> Self {
+        TMino {
+            size: 3,
+            shape: vec![
+                vec![false, true, false],
+                vec![true, true, true],
+                vec![false, false, false],
+            ],
+            color: [0.5, 0.0, 0.5, 1.0],
+        }
+    }
+
+    fn get_size(&self) -> usize {
         self.size
     }
-    pub fn get_shape(&self) -> Vec<Vec<bool>> {
-        // TODO: readonlyな参照を返すだけでもいいかも？(そうすると参照が返ってこないか？)
-        let mut r = vec![vec![false; self.get_size()]; self.get_size()];
-        for i in 0..self.get_size() {
-            for j in 0..self.get_size() {
-                r[i][j] = self.shape[i][j];
-            }
-        }
-        r
+
+    fn get_shape(&self) -> &Vec<Vec<bool>> {
+        &self.shape
     }
-    pub fn get_color(&self) -> [f32; 4] {
+
+    fn get_color(&self) -> [f32; 4] {
         self.color
     }
 }
